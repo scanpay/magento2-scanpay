2bef0ead3b86a7a1f07bd48046211e5fabd68ac6
cblach <cblach@gmail.com>
2016-10-14 21:41:12 +0200

    Initial branch commit

---
+++ Model/Config/ConfigProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Scanpay\PaymentModule\Model\Config;
+
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+
+final class ConfigProvider implements ConfigProviderInterface
+{
+    const CODE = 'scanpaypaymentmodule';
+    public function __construct(
+        \Magento\Payment\Helper\Data $paymentHelper
+    ) {
+        $this->method = $paymentHelper->getMethodInstance(self::CODE);
+    }
+
+    public function getConfig() {
+        return [
+            'payment' => [
+                self::CODE => [
+                    'apikey' =>  $this->method->getConfigData('apikey'),
+                ]
+            ]
+        ];
+    }
+}
