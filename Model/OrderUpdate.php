6519844650ddf9cc90f3f12fbe52d1d693c62b7d
cblach <cblach@gmail.com>
2016-11-02 20:46:56 +0100

    Scanpay Client shifted to zend + now used via dep. inj.), pinghandler updated, order updater upated

--- Model/OrderUpdate.php
+++ Model/OrderUpdate.php
@@ -20,31 +20,52 @@ class OrderUpdater
         $this->$trnBuilder = $trnBuilder;
     }
 
-    public function loadById($orderId) {
-        $this->order->load($orderId);
-        if (!$order->getId()) {
-            return FALSE;
-        }
-        return $this;
+    public function dataIsValid($data)
+    {
+        return isset($data['trnid']) && isset($data['orderid']) &&
+            isset($data['totals']) && isset($data['totals']['authorized']);
     }
 
-    public function update($id, $data)
+    public function update($seq, $data)
     {
+        if (!$this->dataIsValid($data)) {
+            $this->logger->error('Received invalid order data from Scanpay');
+            return false;
+        }
+        $trnId = $data['trnid'];
+        $order = $this->order->load($data['orderid']);
+        /* If order is not in system, ignore it */
+        if (!$order->getId()) {
+            return true;
+        }
         if (!$order->getEmailSent()) {
             $this->orderSender->send($order);
         }
+        $oldSeq = $order->getScanpaySeq();
+        if (isset($oldSeq) && $oldSeq >= $seq) { return; }
+
         $payment = $order->getPayment();
-        $payment->setLastTransId($id);
-        $payment->setTransactionId($id);
+        $payment->setLastTransId($trnId);
+        $payment->setTransactionId($trnId);
 
         $transaction = $this->$trnBuilder->setPayment($payment)->setOrder($order)
             ->setTransactionId($paymentData['id'])->setFailSafe(true)
             ->build(\Magento\Sales\Model\Order\Payment\Transaction::TYPE_AUTH);
-
         $payment->addTransactionCommentsToOrder($transaction, __('The authorized amount is %1.', $data['totals']['authorized']));
         $payment->setParentTransactionId(null);
+        $order->setScanpaySeq($seq);
         $payment->save();
         $order->save();
-        
+        return true;
+    }
+
+    public function updateAll($seq, $dataArr)
+    {
+        foreach ($dataArr as $data) {
+            if (!$this->update($seq, $data)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
