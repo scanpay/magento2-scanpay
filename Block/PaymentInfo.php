9ec545cffcf04963c3f66c7d0f75b87677cd57fd
cblach <cblach@gmail.com>
2016-11-10 19:24:03 +0100

    acts

--- Block/PaymentInfo.php
+++ Block/PaymentInfo.php
@@ -8,10 +8,31 @@ class PaymentInfo extends \Magento\Payment\Block\Info
             return $this->_paymentSpecificInformation;
         }
         $transport = parent::_prepareSpecificInformation($transport);
+        $payment = $this->getInfo();
         $data = [];
-        if ($this->getInfo()->getLastTransId()) {
-            $data[(string) __('Transaction Id')] = $this->getInfo()->getLastTransId();
+        if ($payment->getLastTransId()) {
+            $data[(string) __('Transaction Id')] = $payment->getLastTransId();
         }
-        return $transport->setData(array_merge($data, $transport->getData()));
+        if ($payment->getAmountAuthorized()) {
+            $auth = $payment->getAmountAuthorized();
+            $tot = $payment->getOrder()->getGrandTotal();
+            $fmtAuth = $payment->formatPrice($auth);
+            $fmtTot = $payment->formatPrice($tot);
+            if ($auth < $tot) {
+                $data[(string) __('Partially Authorized')] = __('%1 of %2', $fmtAuth, $fmtTot);
+            } else {
+                $data[(string) __('Authorized')] = $fmtAuth;
+            }
+
+            $paid = $payment->getAmountPaid();
+            if (!$paid) { $paid = 0; }
+            $data[(string) __('Captured')] = $payment->formatPrice($paid);
+
+            $ref = $payment->getAmountRefunded();
+            if ($ref && $ref > 0) {
+                $data[(string) __('Refunded')] = $payment->formatPrice($ref);
+            }
+        }
+        return $transport->addData(array_merge($data, $transport->getData()));
     }
 }
